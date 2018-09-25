179c3380462490c51855736e78c61fba44834d99
user <danielslat98@gmail.com>
2018-09-25 13:48:46 +0300

    20180925_13_48

---
+++ dgr_20180925.py
@@ -0,0 +1,135 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> input("Cienījamais lietotāj, lucu, ievadi vienu skaitli")
+Cienījamais lietotāj, lucu, ievadi vienu skaitli 10
+10
+>>> Mans_mainigais = input("Cienījamais lietotāj, lucu, ievadi vienu skaitli:")
+Cienījamais lietotāj, lucu, ievadi vienu skaitli:10
+>>> mans_mainigais
+
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    mans_mainigais
+NameError: name 'mans_mainigais' is not defined
+>>> Mans_mainigais
+10
+>>> Mans_mainigais = input("Cienījamais lietotāj, lucu, ievadi vienu skaitli:")
+Cienījamais lietotāj, lucu, ievadi vienu skaitli:17
+>>> vars()
+{'__builtins__': <module '__builtin__' (built-in)>, 'Mans_mainigais': 17, '__package__': None, '__name__': '__main__', '__doc__': None}
+>>> type(Mans_mainigais)
+<type 'int'>
+>>> 
+=============== RESTART: /home/user/RTR105/test_1_20180925.py ===============
+Cienījamais lietotāj, lucu, ievadi vienu skaitli: 8
+>>> ars()
+
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    ars()
+NameError: name 'ars' is not defined
+>>> vars()
+{'__builtins__': <module '__builtin__' (built-in)>, '__file__': '/home/user/RTR105/test_1_20180925.py', 'mans_mainigais': 8, '__package__': None, '__name__': '__main__', '__doc__': None}
+>>> 
+=============== RESTART: /home/user/RTR105/test_1_20180925.py ===============
+Cienījamais lietotāj, lucu, ievadi vienu skaitli:8
+>>> vars()
+{'__builtins__': <module '__builtin__' (built-in)>, '__file__': '/home/user/RTR105/test_1_20180925.py', 'mans_mainigais': 8, '__package__': None, 'x': 64, '__name__': '__main__', '__doc__': None}
+>>> 
+=============== RESTART: /home/user/RTR105/test_1_20180925.py ===============
+Cienījamais lietotāj, lucu, ievadi vienu skaitli:8
+>>> vars()
+{'__builtins__': <module '__builtin__' (built-in)>, '__file__': '/home/user/RTR105/test_1_20180925.py', 'mans_mainigais': 8, '__package__': None, 'x': 64, '__name__': '__main__', '__doc__': None}
+>>> 1_20180925.py', 'mans_mainigais': 8, '__package__': None, 'x': 64, '__name__': '__main__', '__doc__': None}
+>>> 
+
+SyntaxError: invalid syntax
+>>> 
+=============== RESTART: /home/user/RTR105/test_1_20180925.py ===============
+Cienījamais lietotāj, lucu, ievadi vienu skaitli:10
+>>> vars()
+{'__builtins__': <module '__builtin__' (built-in)>, '__file__': '/home/user/RTR105/test_1_20180925.py', 'mans_mainigais': 10, '__package__': None, 'x': 100, '__name__': '__main__', '__doc__': None}
+>>> 
+=============== RESTART: /home/user/RTR105/test_1_20180925.py ===============
+Cienījamais lietotāj, lucu, ievadi vienu skaitli:7
+mans_mainigais = 
+>>> 
+=============== RESTART: /home/user/RTR105/test_1_20180925.py ===============
+Cienījamais lietotāj, lucu, ievadi vienu skaitli:21
+mans_mainigais = 21
+>>> vars()
+{'__builtins__': <module '__builtin__' (built-in)>, '__file__': '/home/user/RTR105/test_1_20180925.py', 'mans_mainigais': 21, '__package__': None, 'x': 441, '__name__': '__main__', '__doc__': None}
+>>> 
+=============== RESTART: /home/user/RTR105/test_1_20180925.py ===============
+Cienījamais lietotāj, lucu, ievadi vienu skaitli:232
+mans_mainigais = 232
+vai tu ievadiji = 232
+Tagad atnmiņā mainigasi x ir = 53824
+>>> 
+=============== RESTART: /home/user/RTR105/test_1_20180925.py ===============
+Cienījamais lietotāj, lucu, ievadi vienu skaitli:15468987328686
+mans_mainigais = 15468987328686
+tu ievadiji = 15468987328686
+Tagad atnmiņā mainigasi x ir = 239289568975048030198486596
+>>> 
+=============== RESTART: /home/user/RTR105/test_1_20180925.py ===============
+Cienījamais lietotāj, lucu, ievadi vienu skaitli:13546551354684684654613135415434545487131534564231186461343454343512164684
+mans_mainigais = 13546551354684684654613135415434545487131534564231186461343454343512164684
+tu ievadiji = 13546551354684684654613135415434545487131534564231186461343454343512164684
+Tagad atnmiņā mainigasi x ir = 183509053605109464986493976892543097870072389365035482925442992538512494061123260932248970174917360928630028346313547765563658667364357887536819856
+>>> 
+=============== RESTART: /home/user/RTR105/test_1_20180925.py ===============
+Cienījamais lietotāj, lucu, ievadi dailskaitli:8.1513
+mans_mainigais =      8.151
+tu ievadiji =     8.1513
+Tagad atnmiņā mainigasi x ir =      66.4436917
+>>> 
+=============== RESTART: /home/user/RTR105/test_1_20180925.py ===============
+Cienījamais lietotāj, lucu, ievadi dailskaitli:2.5
+mans_mainigais =      2.500
+tu ievadiji =     2.5000
+Tagad atnmiņā mainigasi x ir =       6.2500000
+Tagad atnmiņā mainigasi x ir =      15.6250000
+Tagad atnmiņā mainigasi x ir =      97.6562500
+>>> 
+=============== RESTART: /home/user/RTR105/test_1_20180925.py ===============
+Cienījamais lietotāj, lucu, ievadi dailskaitli:12.98
+mans_mainigais =     12.980
+tu ievadiji =    12.9800
+Tagad atnmiņā mainigasi x ir =     168.4804000
+Tagad atnmiņā mainigasi y ir =    2186.8755920
+Tagad atnmiņā mainigasi z ir =  368445.6744904
+>>> type(mans_mainigais)
+<type 'float'>
+>>> 
+=============== RESTART: /home/user/RTR105/test_1_20180925.py ===============
+Cienījamais lietotāj, lucu, ievadi simbolu rindu:dfggkkd
+
+Traceback (most recent call last):
+  File "/home/user/RTR105/test_1_20180925.py", line 2, in <module>
+    x = mainigais
+NameError: name 'mainigais' is not defined
+>>> 
+=============== RESTART: /home/user/RTR105/test_1_20180925.py ===============
+Cienījamais lietotāj, lucu, ievadi simbolu rindu:dssds
+mans_mainigais = dssds
+tu ievadiji = dssds
+Tagad atnmiņā mainigasi x ir = dssds
+>>> 
+=============== RESTART: /home/user/RTR105/test_1_20180925.py ===============
+Cienījamais lietotāj, lucu, ievadi simbolu rindu:sa
+mans_mainigais = sa
+tu ievadiji = sa
+
+Traceback (most recent call last):
+  File "/home/user/RTR105/test_1_20180925.py", line 8, in <module>
+    print("Tagad atnmiņā mainigasi x ir = %s"%(x))
+NameError: name 'x' is not defined
+>>> 
+=============== RESTART: /home/user/RTR105/test_1_20180925.py ===============
+Cienījamais lietotāj, lucu, ievadi simbolu rindu:dssd
+mans_mainigais = dssd
+tu ievadiji = dssd
+>>> 
+
---
+++ test_1_20180925.py
@@ -0,0 +1,33 @@
+mans_mainigais = raw_input("Cienījamais lietotāj, lucu, ievadi simbolu rindu:")
+
+
+#x = mans_mainigais * mans_mainigais
+
+print("mans_mainigais = %s"%(mans_mainigais))
+print("tu ievadiji = %s"%(mans_mainigais))
+#print("Tagad atnmiņā mainigasi x ir = %s"%(x))
+
+'''
+mans_mainigais = 1. * input("Cienījamais lietotāj, lucu, ievadi dailskaitli:")
+
+x = mans_mainigais * mans_mainigais
+y = mans_mainigais * x
+z = x * y
+
+print("mans_mainigais = %10.3f"%(mans_mainigais))
+print("tu ievadiji = %10.4f"%(mans_mainigais))
+print("Tagad atnmiņā mainigasi x ir = %15.7f"%(x))
+
+print("Tagad atnmiņā mainigasi y ir = %15.7f"%(y))
+print("Tagad atnmiņā mainigasi z ir = %15.7f"%(z))
+'''
+'''
+mans_mainigais = input("Cienījamais lietotāj, lucu, ievadi vienu skaitli:")
+
+x = mans_mainigais * mans_mainigais
+
+#https://www.cplusplus.com/reference/cstdio/printf/
+print("mans_mainigais = %d"%(mans_mainigais))
+print("tu ievadiji = %d"%(mans_mainigais))
+print("Tagad atnmiņā mainigasi x ir = %d"%(x))
+'''
