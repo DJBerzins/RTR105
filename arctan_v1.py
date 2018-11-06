530223b1c2a583057b74f5a131721abc8c1a7564
user <danielslat98@gmail.com>
2018-11-06 14:06:34 +0200

    20181106

---
+++ arctan_v1.py
@@ -0,0 +1,17 @@
+from math import atan
+
+print("arctan aprēķināšana") 
+x = float(input("Lūdzu ievadiet argument x:"))
+
+y = atan(x)
+print("atan(%.2f) = %.2f"%(x,y))
+
+
+
+print("                    ---")
+print("                  \\")
+print("                   \\")
+print("arctan=x/sqr(1+x*x)") 
+print("                   /")
+print("                  /")
+print("                   ---")
